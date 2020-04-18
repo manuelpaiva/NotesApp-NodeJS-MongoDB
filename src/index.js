13c823c487cdcfb68dc58c07161052b0965afe3f
manup15 <manu.paiva73@gmail.com>
2020-04-18 17:47:48 -0300

    login with passport

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
 
 //initializations
 const app = express()
 require('./database')
+require('./config/passport')
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -28,12 +30,15 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(flash())
 
 //global variables
 app.use((req, res, next) => {
     app.locals.success_msg = req.flash('success_msg')
     app.locals.error_msg = req.flash('error_msg')
+    app.locals.error = req.flash('error')
     next()
 })
 //routes
@@ -44,8 +49,9 @@ app.use(require('./routes/notes'))
 app.get('*',(req, res) => {
     res.status(404).send('the route not exists')
 })
-//static files
-app.use(express.static(path.join(__dirname,'public')))
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 
 app.listen(app.get('port'), () => {
